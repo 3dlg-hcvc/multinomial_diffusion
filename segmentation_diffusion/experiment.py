8f2253b3a12e5dacc6d97bb67b2cfd9bf87f681e
Xiaohao <xsa55@sfu.ca>
2024-05-31 10:04:46 -0700

    Update diffusion model

--- segmentation_diffusion/experiment.py
+++ segmentation_diffusion/experiment.py
@@ -11,6 +11,7 @@ def add_exp_args(parser):
     add_exp_args_parent(parser)
     parser.add_argument('--clip_value', type=float, default=None)
     parser.add_argument('--clip_norm', type=float, default=None)
+    parser.add_argument('--floor_loss', type=eval, default=False)
 
 
 class Experiment(DiffusionExperiment):
@@ -25,8 +26,13 @@ class Experiment(DiffusionExperiment):
             room_type = room_type.to(self.args.device) if torch.sum(room_type) >= 0 else None
             self.optimizer.zero_grad()
             loss_elbo = elbo_bpd(self.model, x.to(self.args.device), floor_plan, room_type)
-            loss_floor = floor_loss(self.model, x.to(self.args.device), floor_plan, room_type)
-            loss = loss_elbo + loss_floor
+            if self.args.floor_loss:
+                loss_floor = floor_loss(self.model, x.to(self.args.device), floor_plan, room_type)
+                loss = loss_elbo + loss_floor
+
+                loss_floor_sum = loss_floor.detach().cpu().item() * len(x)
+            else:
+                loss = loss_elbo
             loss.backward()
             if self.args.clip_value: torch.nn.utils.clip_grad_value_(self.model.parameters(), self.args.clip_value)
             if self.args.clip_norm: torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.args.clip_norm)
@@ -34,17 +40,23 @@ class Experiment(DiffusionExperiment):
             if self.scheduler_iter: self.scheduler_iter.step()
             loss_sum += loss.detach().cpu().item() * len(x)
             loss_count += len(x)
-            loss_floor_sum = loss_floor.detach().cpu().item() * len(x)
-            print('Training. Epoch: {}/{}, Datapoint: {}/{}, Bits/dim: {:.3f}, Floor_loss: {:.3f}'.format(epoch + 1,
-                                                                                                          self.args.epochs,
-                                                                                                          loss_count,
-                                                                                                          len(self.train_loader.dataset),
-                                                                                                          loss_sum / loss_count,
-                                                                                                          loss_floor_sum / loss_count),
-                  end='\r')
+            if self.args.floor_loss:
+                print('Training. Epoch: {}/{}, Datapoint: {}/{}, Bits/dim: {:.3f}, Floor_loss: {:.3f}'.format(epoch + 1,
+                                                                                                              self.args.epochs,
+                                                                                                              loss_count,
+                                                                                                              len(self.train_loader.dataset),
+                                                                                                              loss_sum / loss_count,
+                                                                                                              loss_floor_sum / loss_count),
+                      end='\r')
+            else:
+                print('Training. Epoch: {}/{}, Datapoint: {}/{}, Bits/dim: {:.3f}'.format(epoch + 1, self.args.epochs,
+                                                                                          loss_count,
+                                                                                          len(self.train_loader.dataset),
+                                                                                          loss_sum / loss_count),
+                      end='\r')
         print('')
         if self.scheduler_epoch: self.scheduler_epoch.step()
-        return {'bpd': loss_sum / loss_count, 'floor_loss': loss_floor_sum / loss_count}
+        return {'bpd': loss_sum / loss_count, 'floor_loss': loss_floor_sum / loss_count} if self.args.floor_loss else {'bpd': loss_sum / loss_count}
 
     def eval_fn(self, epoch):
         self.model.eval()
@@ -58,20 +70,34 @@ class Experiment(DiffusionExperiment):
                 room_type = room_type.to(self.args.device) if torch.sum(room_type) >= 0 else None
                 # loss = elbo_bpd(self.model, x.to(self.args.device), floor_plan, room_type)
                 loss_elbo = elbo_bpd(self.model, x.to(self.args.device), floor_plan, room_type)
-                loss_floor = floor_loss(self.model, x.to(self.args.device), floor_plan, room_type)
-                loss = loss_elbo + loss_floor
+                if self.args.floor_loss:
+                    loss_floor = floor_loss(self.model, x.to(self.args.device), floor_plan, room_type)
+                    loss = loss_elbo + loss_floor
+
+                    loss_floor_sum = loss_floor.detach().cpu().item() * len(x)
+                else:
+                    loss = loss_elbo
                 loss_sum += loss.detach().cpu().item() * len(x)
                 loss_count += len(x)
-                loss_floor_sum = loss_floor.detach().cpu().item() * len(x)
-                # print('Train evaluating. Epoch: {}/{}, Datapoint: {}/{}, Bits/dim: {:.3f}'.format(epoch+1, self.args.epochs, loss_count, len(self.train_loader.dataset), loss_sum/loss_count), end='\r')
-                print('Train evaluating. Epoch: {}/{}, Datapoint: {}/{}, Bits/dim: {:.3f}, Floor_loss: {:.3f}'.format(
-                    epoch + 1,
-                    self.args.epochs,
-                    loss_count,
-                    len(self.train_loader.dataset),
-                    loss_sum / loss_count,
-                    loss_floor_sum / loss_count),
-                    end='\r')
+                if self.args.floor_loss:
+                    # print('Train evaluating. Epoch: {}/{}, Datapoint: {}/{}, Bits/dim: {:.3f}'.format(epoch+1, self.args.epochs, loss_count, len(self.train_loader.dataset), loss_sum/loss_count), end='\r')
+                    print(
+                        'Train evaluating. Epoch: {}/{}, Datapoint: {}/{}, Bits/dim: {:.3f}, Floor_loss: {:.3f}'.format(
+                            epoch + 1,
+                            self.args.epochs,
+                            loss_count,
+                            len(self.train_loader.dataset),
+                            loss_sum / loss_count,
+                            loss_floor_sum / loss_count),
+                        end='\r')
+                else:
+                    print('Train evaluating. Epoch: {}/{}, Datapoint: {}/{}, Bits/dim: {:.3f}'.format(
+                        epoch + 1,
+                        self.args.epochs,
+                        loss_count,
+                        len(self.train_loader.dataset),
+                        loss_sum / loss_count),
+                        end='\r')
             print('')
 
         with torch.no_grad():
@@ -83,17 +109,31 @@ class Experiment(DiffusionExperiment):
                 room_type = room_type.to(self.args.device) if torch.sum(room_type) >= 0 else None
                 # loss = elbo_bpd(self.model, x.to(self.args.device), floor_plan, room_type)
                 loss_elbo = elbo_bpd(self.model, x.to(self.args.device), floor_plan, room_type)
-                loss_floor = floor_loss(self.model, x.to(self.args.device), floor_plan, room_type)
+                if self.args.floor_loss:
+                    loss_floor = floor_loss(self.model, x.to(self.args.device), floor_plan, room_type)
+                    loss = loss_elbo + loss_floor
+
+                    loss_floor_sum = loss_floor.detach().cpu().item() * len(x)
+                else:
+                    loss = loss_elbo
                 loss_sum += loss.detach().cpu().item() * len(x)
-                loss_floor_sum = loss_floor.detach().cpu().item() * len(x)
                 loss_count += len(x)
-                print('     Evaluating. Epoch: {}/{}, Datapoint: {}/{}, Bits/dim: {:.3f}, Floor_loss: {:.3f}'.format(
-                    epoch + 1,
-                    self.args.epochs,
-                    loss_count,
-                    len(self.eval_loader.dataset),
-                    loss_sum / loss_count,
-                    loss_floor_sum / loss_count),
-                      end='\r')
+                if self.args.floor_loss:
+                    print(
+                        '     Evaluating. Epoch: {}/{}, Datapoint: {}/{}, Bits/dim: {:.3f}, Floor_loss: {:.3f}'.format(
+                            epoch + 1,
+                            self.args.epochs,
+                            loss_count,
+                            len(self.eval_loader.dataset),
+                            loss_sum / loss_count,
+                            loss_floor_sum / loss_count),
+                        end='\r')
+                else:
+                    print('     Evaluating. Epoch: {}/{}, Datapoint: {}/{}, Bits/dim: {:.3f}'.format(epoch + 1,
+                                                                                                     self.args.epochs,
+                                                                                                     loss_count,
+                                                                                                     len(self.eval_loader.dataset),
+                                                                                                     loss_sum / loss_count),
+                          end='\r')
             print('')
-        return {'bpd': loss_sum / loss_count, 'floor_loss': loss_floor_sum / loss_count}
+        return {'bpd': loss_sum / loss_count, 'floor_loss': loss_floor_sum / loss_count} if self.args.floor_loss else {'bpd': loss_sum / loss_count}
